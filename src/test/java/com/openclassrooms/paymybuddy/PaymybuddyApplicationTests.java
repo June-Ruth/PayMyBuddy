8dd57a99a006003f6b186c493b80821d9d339ee4
June-Ruth <n-barral@hotmail.fr>
2021-01-27 10:55:14 +0100

    introduce security with OC

--- src/test/java/com/openclassrooms/paymybuddy/PaymybuddyApplicationTests.java
+++ src/test/java/com/openclassrooms/paymybuddy/PaymybuddyApplicationTests.java
@@ -1,13 +1,25 @@
 package com.openclassrooms.paymybuddy;
 
+import com.openclassrooms.paymybuddy.login.controller.LoginController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 
 @SpringBootTest
+@ExtendWith(SpringExtension.class)
 class PaymybuddyApplicationTests {
 
+	@Autowired
+	private LoginController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertNotNull(controller);
 	}
 
 }
