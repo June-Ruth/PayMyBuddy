2a979ef9476f81d2bc71dfac6c51187e7b24eaa6
June-Ruth <n-barral@hotmail.fr>
2021-02-22 16:35:44 +0100

    add role to model

---
+++ src/main/java/com/openclassrooms/paymybuddy/model/RoleProfile.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.paymybuddy.model;
+
+public enum RoleProfil {
+    /**
+     * Security role for user.
+     */
+    USER,
+    /**
+     * Security role for admin only.
+     */
+    ADMIN
+}
